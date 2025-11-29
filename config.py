633eca9c6177a948b1627851591f4ae46a1189f4
vinit000033 <vinit000033@gmail.com>
2025-11-29 23:13:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-api_id = "api id"
-api_hash = "api hadh"
+api_id = "28924276"
+api_hash = "790f4c3f5f1de21ab91e6f32d0e3c803"
 bot_token = "bot token"
 auth_users = [7890781002]
 sudo_users = [7890781002]
