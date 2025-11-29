9238c092becb95f7538c1d05a77dfbe0a174012c
vinit000033 <vinit000033@gmail.com>
2025-11-29 23:21:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 api_id = "28924276"
 api_hash = "790f4c3f5f1de21ab91e6f32d0e3c803"
-bot_token = "bot token"
-auth_users = [7890781002]
-sudo_users = [7890781002]
+bot_token = "8486682784:AAG3qiB-kKvA5qRJKUKC5OkcE3cojkhca5w"
+auth_users = [8444919166]
+sudo_users = [8444919166]
